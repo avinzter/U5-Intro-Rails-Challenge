5b48f1cc50b19bdffaeb1e4bda3eda77643dd4e5
Geniva B. Lacara <geniva.lacara@gmail.com>
2013-10-25 13:10:45 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsChallenge::Application.config.secret_token = 'ce7b238e6fbf2dc77489e5add4b00fcf639a24e62da3f10d4da3761c290b0b81251f20c57d11b5594236993d254908a4be1a7366497e27f06c40857dc84feb8f'
